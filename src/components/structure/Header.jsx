3ce904621e6a6e407c5900fdf3569ddb840f7702
Issa71 <mohamedissa95@gmail.com>
2022-09-02 17:58:40 +0100

    first commit

---
+++ src/components/structure/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import SearchBar from './Searchbar';
+
+const Header = (props) => {
+
+    return (
+        <div className="header">
+            <div className="logo"><Link to='/'><img alt="Logo" src="https://cdn.discordapp.com/attachments/768054278933184533/1014837355909226497/qa-cinemas-logo-transp.png" width={70} height={45}/></Link></div>
+            <div className="nav">
+                <div className="navLinks">
+                    <Link to='/'>Home</Link>
+                    <Link to='/listings'>Now Showing</Link>
+                    <Link to='/newReleases'>New Releases</Link>
+                    <Link to='/bookNow'>Book Now</Link>
+                    <Link to='/payForTickets'>Pay For Tickets</Link>
+                    <Link to='/classifications'>Movie Classifications</Link>
+                    <Link to='/placesToGo'>Places To Go</Link>
+                    <Link to='/discussionBoard'>Discussion Board</Link>
+                    <Link to='/screens'>Screens</Link>
+                    <Link to='/contact'>Contact Us</Link>
+                    <Link to='/openingTimes'>Opening Times</Link>
+                    <Link to='/travelPage'>Travel Page</Link>
+                    <Link to='/aboutPage'>About Page</Link>
+                </div>
+                <SearchBar setSearchResult={props.setSearchResult} />
+            </div>
+        </div>
+    );
+
+};
+
+export default Header;
